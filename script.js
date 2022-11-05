34c97b4d22c4e19fb543a98c767547ca70de25a0
Maczeta5 <chmielecki.michall@gmail.com>
2022-11-05 12:45:39 +0100

    new files

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const searchInput = document.querySelector('.search-input')
-const searchBtn = document.querySelector('.search-btn')
+const btn = document.querySelector('.btn')
 
+const btnAnimation = e => {
+    const top = e.clientY
+    const left = e.clientX
+    // pozycja, w którą klikamy
 
-const showSearchBar = () => {
-    searchInput.classList.toggle('active')
+    const btnTopPosition = e.target.offsetTop
+    const btnLeftPosition = e.target.offsetLeft
+    // pozycja przycisku
+
+    const insideBtnTop = top - btnTopPosition
+    const insideBtnLeft = left - btnLeftPosition
+
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = insideBtnTop + 'px'
+    circle.style.left = insideBtnLeft + 'px'
+
+    e.target.appendChild(circle)
+
+    setTimeout(() => {
+        circle.remove()
+    }, 300);
 }
 
-searchBtn.addEventListener('click', showSearchBar)
+
+btn.addEventListener('click', btnAnimation)
